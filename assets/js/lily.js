90f2812b176daf981c37832be107737a6acb0881
HowerZiu <howerziu@yeah.net>
2023-04-05 21:37:33 +0800

    Store light mode state in localStorage

--- assets/js/lily.js
+++ assets/js/lily.js
@@ -1,28 +1,8 @@
-function setLilyCookie(cname,cvalue,exdays,path) {
-    var d=new Date();
-    d.setTime(d.getTime()+(exdays*24*60*60*1000));
-    c=cname+"="+cvalue;
-    if (exdays) c=c+"; expires="+d.toGMTString();
-    if (path) c=c+"; path="+path;
-    document.cookie=c;
-}
-function getLilyCookie(cname) {
-    var name=cname+"=";
-    var carr=document.cookie.split(';');
-    for (var i=0; i<carr.length; ++i) {
-        var c=carr[i].trim();
-        if (c.indexOf(name)==0) return c.substring(name.length,c.length);
-    }
-    return "";
-}
-function delLilyCookie(cname) {
-    document.cookie=cname+"=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
-}
-function setLilyMode() {
+function setLightMode() {
     body=document.body;
-    var lily_mode=getLilyCookie("lily_mode");
+    var light_mode=localStorage.getItem("light_mode");
     var theme_color_meta=document.querySelector('meta[name="theme-color"]');
-    if (lily_mode=="dark") {
+    if (light_mode=="dark") {
         body.classList.remove("light");
         body.classList.add("dark");
         document.getElementById("body").classList.remove("lily-mode-light");
@@ -36,20 +16,20 @@ function setLilyMode() {
         theme_color_meta.setAttribute("content","#dee0ff");
     }
 }
-function changeLilyMode() {
-    var lily_mode=getLilyCookie('lily_mode');
-    if (lily_mode=='dark')
-        lily_mode='light';
+function changeLightMode() {
+    var light_mode=localStorage.getItem("light_mode");
+    if (light_mode=='dark')
+        light_mode='light';
     else
-        lily_mode='dark';
-    setLilyCookie('lily_mode',lily_mode,180,'/');
-    setLilyMode();
+        light_mode='dark';
+    localStorage.setItem('light_mode',light_mode);
+    setLightMode();
     document.body.classList.add("transition");
     window.setTimeout(()=>{
         document.body.classList.remove("transition");
     },500);
 }
-function lilyScrollTop() {
+function scrollToTop() {
     var elm=document.body||document.documentElement;
     elm.scrollIntoView();
 }
